3bdcb58493d8f2f5aa704c10e45cad15d7a424ea
Priyanka Priyadarshini Ghadai <144894639+ppdr2002@users.noreply.github.com>
2024-12-03 11:45:32 +0530

    nav-update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -52,88 +53,100 @@ export default function Navbar() {
             <div>
               <div className="group px-4 py-2">
                 Services
-                <div className="group-hover:grid grid-cols-3 hidden absolute my-8 mx-14 left-0 right-0 top-14 bottom-5 z-10 p-6 bg-blue-900 text-white h-fit gap-8">
-                  {/* Strategy Section */}
-                  <div className="flex flex-col gap-4">
-                    <Link href="/strategy" className="hover:underline font-bold text-base"> Strategy </Link>
-                    <p className="font-light text-sm">Technology Strategy</p>
-                    <p className="font-light text-sm">Data Strategy</p>
-                    <p className="font-light text-sm">
-                      Innovation & Emerging Technology Strategy
-                    </p>
+                <div className="group-hover:flex hidden items-start justify-between absolute my-8 mx-14 left-0 right-0 top-14 bottom-5 z-10 p-6 bg-white text-black h-[570px]">
+                  {/* DROPDOWN 1st SECTION */}
+                  <div className="flex flex-col justify-between w-[280px] h-full">
+                    <div className="flex flex-col gap-4">
+                      <h1 className="text-4xl flex flex-wrap gap-2 font-semibold">Made to <p className="text-blue-600">Scale</p></h1>
+                      <p className="text-wrap">Our software development services are built to evolve your business idea into a successful growth story</p>
+                      <div className="border">
+                        <Image src={"/"} alt="IMAGE" height={100} width={100}/>
+                      </div>                      
+                    </div>
+                    {/* Startup Development Section */}
+                    <div className="flex flex-col gap-4 font-bold">
+                      <Link href="/#" className="hover:underline">Startup Software Development</Link>
+                      <Link href="/#" className="hover:underline">SaaS Development</Link>
+                      <Link href="/#" className="hover:underline">Supply Chain Management</Link>
+                    </div>                 
                   </div>
 
-                  {/* Cloud Section */}
-                  <div className="flex flex-col gap-4">
-                    <Link href="/#" className="hover:underline font-bold text-base">
-                      Cloud
-                    </Link>
-                    <p className="font-light text-sm">Migrations & Modernization</p>
-                    <p className="font-light text-sm">Optimization</p>
-                    <p className="font-light text-sm">Infrastructure</p>
+                  {/* DROPDOWN 2nd SECTION */}
+                  <div className="flex flex-col justify-between w-[280px] h-full">
+                    {/* Strategy Section */}
+                    <div className="flex flex-col gap-4">
+                      <Link href="/strategy" className="hover:underline font-bold text-lg"> Strategy </Link>
+                      <p className="font-light text-sm">Technology Strategy</p>
+                      <p className="font-light text-sm">Data Strategy</p>
+                      <p className="font-light text-sm">Innovation & Emerging Technology Strategy</p>
+                    </div>
+
+                    <div className="border opacity-30"></div>
+
+                    {/* Cloud Section */}
+                    <div className="flex flex-col gap-4">
+                      <Link href="/#" className="hover:underline font-bold text-lg">Cloud</Link>
+                      <p className="font-light text-sm">Migrations & Modernization</p>
+                      <p className="font-light text-sm">Optimization</p>
+                      <p className="font-light text-sm">Infrastructure</p>
+                    </div>
+
+                    <div className="border opacity-30"></div>
+
+                    {/* Digital Product Building Section */}
+                    <div className="flex flex-col gap-4">
+                      <Link href="/#" className="hover:underline font-bold text-lg">Digital Product Building</Link>
+                      <p className="font-light text-sm">Product Engineering</p>
+                      <p className="font-light text-sm">Platform Engineering</p>
+                      <p className="font-light text-sm">Data Engineering</p>
+                    </div>
                   </div>
 
-                  {/* Digital Product Building Section */}
-                  <div className="flex flex-col gap-4">
-                    <Link href="/#" className="hover:underline font-bold text-base">
-                      Digital Product Building
-                    </Link>
-                    <p className="font-light text-sm">Product Engineering</p>
-                    <p className="font-light text-sm">Platform Engineering</p>
-                    <p className="font-light text-sm">Data Engineering</p>
+                  {/* DROPDOWN 3rd SECTION */}                
+                  <div className="flex flex-col gap-10 w-[280px] h-full">
+                    {/* Artificial Intelligence Section */}
+                    <div className="flex flex-col gap-3">
+                      <Link href="/#" className="hover:underline font-bold text-lg">
+                        Artificial Intelligence
+                      </Link>
+                      <p className="font-light text-sm">Machine Learning</p>
+                      <p className="font-light text-sm">Generative AI</p>
+                      <p className="font-light text-sm">Intelligent Products</p>
+                    </div>
+
+                    <div className="border opacity-30"></div>
+
+                    {/* Experience Strategy and Design Section */}
+                    <div className="flex flex-col gap-4">
+                      <Link href="/#" className="hover:underline font-bold text-lg">Experience Strategy and Design</Link>
+                      <p className="font-light text-sm">Customer & User Experience</p>
+                      <p className="font-light text-sm">Product Strategy, Design & Innovation</p>
+                      <p className="font-light text-sm">Service Design</p>
+                      <p className="font-light text-sm">Brand Strategy</p>
+                    </div>
                   </div>
 
-                  {/* Artificial Intelligence Section */}
-                  <div className="flex flex-col gap-4">
-                    <Link href="/#" className="hover:underline font-bold text-base">
-                      Artificial Intelligence
-                    </Link>
-                    <p className="font-light text-sm">Machine Learning</p>
-                    <p className="font-light text-sm">Generative AI</p>
-                    <p className="font-light text-sm">Intelligent Products</p>
-                  </div>
+                  {/* DROPDOWN 4th SECTION */}            
+                  <div className="flex flex-col gap-7 w-[280px] h-full">
+                    {/* Software Development Section */}
+                    <div className="flex flex-col gap-4">
+                      <Link href="/#" className="hover:underline font-bold text-lg">Software Development</Link>
+                      <p className="font-light text-sm">UI/UX</p>
+                      <p className="font-light text-sm">Mobile Development</p>
+                      <p className="font-light text-sm">Web Development</p>
+                    </div>
 
-                  {/* Experience Strategy and Design Section */}
-                  <div className="flex flex-col gap-4">
-                    <Link
-                      href="/#"
-                      className="hover:underline font-bold text-base"
-                    >
-                      Experience Strategy and Design
-                    </Link>
-                    <p className="font-light text-sm">Customer & User Experience</p>
-                    <p className="font-light text-sm">Product Strategy, Design & Innovation</p>
-                    <p className="font-light text-sm">Service Design</p>
-                    <p className="font-light text-sm">Brand Strategy</p>
-                  </div>
+                    <div className="border opacity-30"></div>
 
-                  {/* Digital Transformation Services Section */}
-                  <div className="flex flex-col gap-4">
-                    <Link href="/#" className="hover:underline font-bold text-base">
-                      Digital Transformation Services
-                    </Link>
-                    <p className="font-light text-sm">Strategy & Journey</p>
-                    <p className="font-light text-sm">Digital Applications</p>
-                    <p className="font-light text-sm">AI Automation</p>
-                    <p className="font-light text-sm">Legacy Modernisation</p>
-                  </div>
-
-                  {/* Startup Development Section */}
-                  <div className="flex flex-col gap-4 font-bold">
-                    <Link href="/#" className="hover:underline">Startup Software Development</Link>
-                    <Link href="/#" className="hover:underline">SaaS Development</Link>
-                    <Link href="/#" className="hover:underline">Supply Chain Management</Link>
-                  </div>
-
-                  {/* Software Development Section */}
-                  <div className="flex flex-col gap-4">
-                    <Link href="/#" className="hover:underline font-bold text-base">
-                      Software Development
-                    </Link>
-                    <p className="font-light text-sm">UI/UX</p>
-                    <p className="font-light text-sm">Mobile Development</p>
-                    <p className="font-light text-sm">Web Development</p>
-                  </div>
+                    {/* Digital Transformation Services Section */}
+                    <div className="flex flex-col gap-4">
+                      <Link href="/#" className="hover:underline font-bold text-lg">Digital Transformation Services</Link>
+                      <p className="font-light text-sm">Strategy & Journey</p>
+                      <p className="font-light text-sm">Digital Applications</p>
+                      <p className="font-light text-sm">AI Automation</p>
+                      <p className="font-light text-sm">Legacy Modernisation</p>
+                    </div>
+                  </div>                  
                 </div>
               </div>
             </div>
